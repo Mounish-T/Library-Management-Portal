6c37e680d558f16bcac881b5136bcee48998f14b
Mounish-T <tmounish02@gmail.com>
2026-01-03 09:19:23 +0530

    Initial setup for librarian panel

---
+++ librarian/js/librarian-dashboard.js
@@ -0,0 +1,12 @@
+// Activate the color in side-nav for current page 
+const navLinks = document.querySelectorAll('.side-nav .activities a');
+navLinks[0].classList.add('active');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // prevent default navigation for demo
+        navLinks.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+        window.location.href = link.getAttribute('href');
+    });
+});
